7252c90a96e87cf57c2103d72723fdd585946a06
Yihan <wuyihan@u.nus.edu>
2023-09-25 14:17:43 +0200

    add README

--- examples/Silicon/_aiidasubmit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-exec > _scheduler-stdout.txt
-exec 2> _scheduler-stderr.txt
-
-
-#export the project path
-export JULIA_PROJECT="/home/max/Desktop/Aiida_DFTK_Test/AiidaDFTK/AiidaDFTK.jl"
-
-"/usr/bin/julia" "-e" "using AiidaDFTK; AiidaDFTK.run()" "DFTK.json"  > 'DFTK.txt' 
