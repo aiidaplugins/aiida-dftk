da707c6c0275bdac39323acca04f97376c6ae482
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-11-06 17:19:50 +0100

    Disable MPI in tests

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -41,6 +41,10 @@ def test_silicon_workflow(get_dftk_code, generate_structure, generate_kpoints_me
         ]
     })
 
+    # Disable MPI for now. If we ever enable MPI,
+    # we'll need to make sure that tests can run on both GitHub Actions and locally without (too much) special setup.
+    builder.dftk.metadata.options.withmpi = False
+
     result = submit_and_await_success(builder, timeout=180)
 
     assert result.outputs.output_parameters.get_dict()["converged"]
